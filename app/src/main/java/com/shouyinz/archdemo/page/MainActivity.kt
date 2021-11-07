9f37be919447bfd6b3378a6bb9c63743ffce9740
shouyinz <justin.tsai@linebank.com.tw>
2021-11-07 10:18:47 +0800

    Add basic navigation scaffold

--- app/src/main/java/com/shouyinz/archdemo/page/MainActivity.kt
+++ app/src/main/java/com/shouyinz/archdemo/page/MainActivity.kt
@@ -1,11 +1,23 @@
-package com.shouyinz.archdemo
+package com.shouyinz.archdemo.page
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.shouyinz.archdemo.R
+import com.shouyinz.archdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val navController: NavController by lazy {
+        findNavController(R.id.host_fragment)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController.setGraph(R.navigation.main_nav_graph)
     }
 }
